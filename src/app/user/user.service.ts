b18aad9f5ac53391d3512acc5ae603de951dd707
Aslıhan Hasar <aslihan_hasar@hotmail.com>
2023-07-13 22:43:53 +0300

    Add user methods

--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { User } from './user';
+import { users } from 'src/assets/userpool';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
+
+  getUsers():User[]{
+    return users;
+  }
+
   constructor() { }
+
+   getUserProperties(): string[] {
+    return Object.keys(users[0]);
+  }
+  
 }
