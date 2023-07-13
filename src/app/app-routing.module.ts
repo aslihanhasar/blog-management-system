dc5b04c4fc706dd1bd023da0f39990a99a3bdb71
Aslıhan Hasar <aslihan_hasar@hotmail.com>
2023-07-13 21:21:13 +0300

    Set user routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserListComponent } from './user/user-list/user-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"users",component:UserListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
