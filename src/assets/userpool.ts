6233ec9d7094f5a4c2aa88c2eb4f1423b3f51444
Aslıhan Hasar <aslihan_hasar@hotmail.com>
2023-07-13 21:37:29 +0300

    Add default users

---
+++ src/assets/userpool.ts
@@ -0,0 +1,212 @@
+export const userPool=[
+    {
+      userId: 1,
+      username: "aglassopp0",
+      email: "apharaoh0@elegantthemes.com",
+      creationDate: "15/07/2018",
+      isActive: false
+    },
+    {
+      userId: 2,
+      username: "gdessant1",
+      email: "nduthie1@mapquest.com",
+      creationDate: "13/05/2011",
+      isActive: false
+    },
+    {
+      userId: 3,
+      username: "kdelbergue2",
+      email: "nthinn2@tinypic.com",
+      creationDate: "18/06/2012",
+      isActive: false
+    },
+    {
+      userId: 4,
+      username: "cheadland3",
+      email: "ssanderson3@icq.com",
+      creationDate: "08/01/2020",
+      isActive: true
+    },
+    {
+      userId: 5,
+      username: "cfrill4",
+      email: "bsabberton4@bbc.co.uk",
+      creationDate: "01/01/2013",
+      isActive: false
+    },
+    {
+      userId: 6,
+      username: "dlanfear5",
+      email: "lebbings5@sciencedaily.com",
+      creationDate: "15/09/2015",
+      isActive: false
+    },
+    {
+      userId: 7,
+      username: "rgallamore6",
+      email: "givanshintsev6@guardian.co.uk",
+      creationDate: "26/04/2016",
+      isActive: false
+    },
+    {
+      userId: 8,
+      username: "wmuskett7",
+      email: "hassiratti7@gov.uk",
+      creationDate: "23/07/2017",
+      isActive: true
+    },
+    {
+      userId: 9,
+      username: "jfelder8",
+      email: "sshoebotham8@loc.gov",
+      creationDate: "03/05/2011",
+      isActive: true
+    },
+    {
+      userId: 10,
+      username: "rcastagno9",
+      email: "aglamart9@tripadvisor.com",
+      creationDate: "17/11/2011",
+      isActive: false
+    },
+    {
+      userId: 11,
+      username: "rolmana",
+      email: "gaustinsa@bizjournals.com",
+      creationDate: "05/08/2021",
+      isActive: false
+    },
+    {
+      userId: 12,
+      username: "smcalindonb",
+      email: "awaplesb@dmoz.org",
+      creationDate: "17/12/2020",
+      isActive: true
+    },
+    {
+      userId: 13,
+      username: "mlangmaidc",
+      email: "habelc@adobe.com",
+      creationDate: "16/10/2020",
+      isActive: true
+    },
+    {
+      userId: 14,
+      username: "elaised",
+      email: "llillymand@yellowpages.com",
+      creationDate: "17/03/2022",
+      isActive: false
+    },
+    {
+      userId: 15,
+      username: "abonomee",
+      email: "sradouxe@intel.com",
+      creationDate: "25/04/2013",
+      isActive: false
+    },
+    {
+      userId: 16,
+      username: "gyarrowf",
+      email: "chenkenf@tuttocitta.it",
+      creationDate: "16/10/2010",
+      isActive: false
+    },
+    {
+      userId: 17,
+      username: "fcumberg",
+      email: "mchurng@dailymail.co.uk",
+      creationDate: "02/06/2013",
+      isActive: true
+    },
+    {
+      userId: 18,
+      username: "bcolecroughh",
+      email: "aunderhillh@weibo.com",
+      creationDate: "16/02/2018",
+      isActive: true
+    },
+    {
+      userId: 19,
+      username: "bfetherbyi",
+      email: "dchristoffei@histats.com",
+      creationDate: "01/04/2013",
+      isActive: true
+    },
+    {
+      userId: 20,
+      username: "amuffj",
+      email: "mbaldenj@ibm.com",
+      creationDate: "20/05/2019",
+      isActive: true
+    },
+    {
+      userId: 21,
+      username: "kderechterk",
+      email: "tfoordk@columbia.edu",
+      creationDate: "12/03/2011",
+      isActive: true
+    },
+    {
+      userId: 22,
+      username: "kprevettl",
+      email: "shavenhandl@paginegialle.it",
+      creationDate: "09/06/2011",
+      isActive: false
+    },
+    {
+      userId: 23,
+      username: "tbarnfieldm",
+      email: "ekinkadem@sun.com",
+      creationDate: "06/02/2014",
+      isActive: false
+    },
+    {
+      userId: 24,
+      username: "tboissiern",
+      email: "kgreern@wp.com",
+      creationDate: "24/04/2021",
+      isActive: false
+    },
+    {
+      userId: 25,
+      username: "jnapolio",
+      email: "mwhittingtono@uiuc.edu",
+      creationDate: "29/07/2011",
+      isActive: true
+    },
+    {
+      userId: 26,
+      username: "bgibbinp",
+      email: "nlosseljongp@bandcamp.com",
+      creationDate: "23/10/2013",
+      isActive: false
+    },
+    {
+      userId: 27,
+      username: "pbonnq",
+      email: "cpenbarthyq@webeden.co.uk",
+      creationDate: "20/12/2010",
+      isActive: true
+    },
+    {
+      userId: 28,
+      username: "lmenierr",
+      email: "pboiser@seattletimes.com",
+      creationDate: "09/04/2013",
+      isActive: true
+    },
+    {
+      userId: 29,
+      username: "blunos",
+      email: "rbukowskis@ucoz.ru",
+      creationDate: "29/09/2015",
+      isActive: true
+    },
+    {
+      userId: 30,
+      username: "ablaszczykt",
+      email: "calfordet@reference.com",
+      creationDate: "25/04/2016",
+      isActive: true
+    }
+  ]
