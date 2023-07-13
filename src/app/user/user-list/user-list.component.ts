9a69d00851fa566fd521d98e69f6dbc17c00adcf
Aslıhan Hasar <aslihan_hasar@hotmail.com>
2023-07-13 23:04:34 +0300

    Add codes to get users data

--- src/app/user/user-list/user-list.component.ts
+++ src/app/user/user-list/user-list.component.ts
@@ -1,11 +1,20 @@
-import {Component } from '@angular/core';
-
+import {Component,OnInit } from '@angular/core';
+import { User } from '../user';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.css']
 })
-export class UserListComponent {
+export class UserListComponent implements OnInit {
+  users:User[]=[];
+  tableColumns:string[]=[];
 
+  constructor(private userService:UserService){}
+
+  ngOnInit(): void {
+      this.users=this.userService.getUsers();
+      this.tableColumns=this.userService.getUserProperties();
+  }
 }
