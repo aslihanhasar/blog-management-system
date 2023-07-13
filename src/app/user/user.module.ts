bdd88e39fcc3bd9f213756dbd90b1435e359dd35
Aslıhan Hasar <aslihan_hasar@hotmail.com>
2023-07-13 20:01:14 +0300

    Create list and add components

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserAddComponent } from './user-add/user-add.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    UserListComponent,
+    UserAddComponent
+  ],
   imports: [
     CommonModule
   ]
