a9ef4c8d895797878146becfb9750dc1d1ec2986
Aslıhan Hasar <aslihan_hasar@hotmail.com>
2023-07-13 23:35:39 +0300

    Add all components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { UserListComponent } from './user/user-list/user-list.component';
+import { UserService } from './user/user.service';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
