d50194359b5d36a64904e3eece425299caf39a1b
Aslıhan Hasar <aslihan_hasar@hotmail.com>
2023-07-15 03:48:30 +0300

    Update isDataUnique() and delete get methods

--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -17,18 +17,6 @@ export class UserService {
     return Object.keys(users[0]);
   }
  
-  getUserById(id: Number): User | undefined {
-    return this.getUsers().find(u => u.userId === id);
-  }
-
-  getUserByUsername(username: string): User| undefined{
-    return this.getUsers().find(u=> u.username === username )
-  }
-
-  getUserByEmail(email: string): User| undefined{
-    return this.getUsers().find(u=> u.email === email )
-  }
-
   deleteUser(id: Number):void {
     const index = users.findIndex(u => u.userId === id);
     if (index !== -1) {
@@ -47,11 +35,9 @@ export class UserService {
     const lowerCaseUsername = username.toLowerCase();
     const lowerCaseEmail = email.toLowerCase();
 
-    if(this.getUsers().find((user) => user.username.toLowerCase()!==undefined && this.getUserByUsername(lowerCaseUsername)!.userId !== id)) 
-      return false;
-    else if(this.getUsers().find((user) => user.email.toLowerCase() !== undefined && this.getUserByEmail(lowerCaseEmail)!.userId !== id))
-      return false;
-    else
-      return true;
+    const duplicateUsername = this.getUsers().find((user) => user.username.toLowerCase() === lowerCaseUsername && user.userId !== id);
+    const duplicateEmail = this.getUsers().find((user) => user.email.toLowerCase() === lowerCaseEmail && user.userId !== id);
+
+    return !duplicateUsername && !duplicateEmail;
   }
 }
