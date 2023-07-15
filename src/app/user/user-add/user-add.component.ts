76bf834adaf66c79d2ee310cfa8cc151dfa9ed7c
Aslıhan Hasar <aslihan_hasar@hotmail.com>
2023-07-15 04:54:32 +0300

    Add a method to create new user

--- src/app/user/user-add/user-add.component.ts
+++ src/app/user/user-add/user-add.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { User } from '../user';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-user-add',
@@ -16,6 +17,33 @@ export class UserAddComponent {
     isActive: false
   }
 
-  
+  constructor(private userService: UserService) {}
+
+  performCreate(){
+    if (this.user.username === '' || this.user.email === '' || this.user.creationDate === '') {
+      alert('All information must be filled.');
+      return;
+    }
+    const newUser: User = {
+      userId: this.userService.generateUserId(),
+      username: this.user.username,
+      email: this.user.email,
+      creationDate: this.user.creationDate,
+      isActive: this.user.isActive
+    };
+
+    if (!this.userService.isDataUnique(this.user.username, this.user.email, newUser.userId)) {
+      alert('Username or email already exist.');
+      return;
+    }
+
+    this.userService.addUser(newUser);
+  }
+
+  performCancel(){
+
+  }
+
+
 
 }
