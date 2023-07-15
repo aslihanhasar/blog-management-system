1ed6655097db7b63bcbf83bd06c71bc296249b5f
Aslıhan Hasar <aslihan_hasar@hotmail.com>
2023-07-15 18:11:29 +0300

    Add route path for post add and detail

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { UserListComponent } from './user/user-list/user-list.component';
 import { UserAddComponent } from './user/user-add/user-add.component';
 import { PostListComponent } from './post/post-list/post-list.component';
+import { PostAddComponent } from './post/post-add/post-add.component';
+import { PostDetailComponent } from './post/post-detail/post-detail.component';
+
 
 const routes: Routes = [
   {
@@ -13,6 +16,12 @@ const routes: Routes = [
   },
   {
     path: "posts", component: PostListComponent
+  },
+  {
+    path: "postadd", component: PostAddComponent
+  },
+  {
+    path: "postlist/:id", component: PostDetailComponent
   },
 ];
 
