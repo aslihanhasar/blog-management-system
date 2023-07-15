bb2a3c3f6c8dfec12f87b26366fbef0ab6a1b4e7
Aslıhan Hasar <aslihan_hasar@hotmail.com>
2023-07-15 03:57:25 +0300

    Route user-add-component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserListComponent } from './user/user-list/user-list.component';
+import { UserAddComponent } from './user/user-add/user-add.component';
 
 const routes: Routes = [
   {
     path:"users",component:UserListComponent
-  }
+  },
+  {
+    path: "adduser", component: UserAddComponent
+  },
 ];
 
 @NgModule({
