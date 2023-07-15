b70798fc031112d0465675248706bdac4d7fd454
Aslıhan Hasar <aslihan_hasar@hotmail.com>
2023-07-15 18:44:24 +0300

    Add details section

--- src/app/post/post-detail/post-detail.component.ts
+++ src/app/post/post-detail/post-detail.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Post } from '../post';
+import { PostService } from '../post.service';
 
 @Component({
   selector: 'app-post-detail',
   templateUrl: './post-detail.component.html',
   styleUrls: ['./post-detail.component.css']
 })
-export class PostDetailComponent {
+export class PostDetailComponent implements OnInit{
+  post: Post | undefined;
+
+  constructor(
+    private route: ActivatedRoute,
+    private postService: PostService
+  ) { }
+
+  ngOnInit(): void {
+    const postId = Number(this.route.snapshot.paramMap.get('id'));
+    this.post = this.postService.getPostById(postId);
+  }
+
+  
 
 }
