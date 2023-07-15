a39babec3349db92bc9989f8aaff639638f37e9c
Aslıhan Hasar <aslihan_hasar@hotmail.com>
2023-07-15 16:17:09 +0300

    Add routing for post

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserListComponent } from './user/user-list/user-list.component';
 import { UserAddComponent } from './user/user-add/user-add.component';
+import { PostListComponent } from './post/post-list/post-list.component';
 
 const routes: Routes = [
   {
@@ -9,6 +10,9 @@ const routes: Routes = [
   },
   {
     path: "adduser", component: UserAddComponent
+  },
+  {
+    path: "posts", component: PostListComponent
   },
 ];
 
