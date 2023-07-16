e0cc785d97e452d590955ce2e224663894ae4956
Aslıhan Hasar <aslihan_hasar@hotmail.com>
2023-07-16 15:46:41 +0300

    Create interface

--- src/app/comment/comment.ts
+++ src/app/comment/comment.ts
@@ -1,2 +1,9 @@
 export interface Comment {
+    [key: string]: any;
+    commentId: Number,
+    postId: Number,
+    userId: Number,
+    comment: string,
+    creationDate: string,
+    isConfirmed: boolean
 }
