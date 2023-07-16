674ee287b8363f67da9e78d964da4f102ebc74c2
Aslıhan Hasar <aslihan_hasar@hotmail.com>
2023-07-16 17:16:16 +0300

    Create comment detail operations

--- src/app/comment/comment-detail/comment-detail.component.ts
+++ src/app/comment/comment-detail/comment-detail.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { ActivatedRoute} from '@angular/router';
+import { Comment } from '../comment';
+import { CommentService } from '../comment.service';
 
 @Component({
   selector: 'app-comment-detail',
@@ -6,5 +9,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./comment-detail.component.css']
 })
 export class CommentDetailComponent {
+  comment:Comment | undefined;
+  comments: Comment[] = [];
 
+  constructor(private commentService: CommentService, private route: ActivatedRoute) {
+  }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.comments = this.commentService.getComments();
+      const id = params['id'];
+      const commentId = parseInt(id, 10);
+      this.comment = this.comments.find(comment => comment.commentId === commentId)!;
+    });
+  }
 }
