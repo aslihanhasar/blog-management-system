48bcf6abb00711052ed3e4201f235d724fd16c18
Aslıhan Hasar <aslihan_hasar@hotmail.com>
2023-07-16 05:54:48 +0300

    Create categories data

---
+++ src/assets/categorypool.ts
@@ -0,0 +1,56 @@
+import { Category } from "src/app/category/category";
+
+export const categories: Category[] = [
+
+    {
+        categoryId: 1,
+        name: "fulfillment",
+        creationDate: "2018-10-11"
+    },
+    {
+        categoryId: 2,
+        name: "gratitude",
+        creationDate: "2015-06-04"
+    },
+    {
+        categoryId: 3,
+        name: "growth",
+        creationDate: "2014-06-01"
+    },
+    {
+        categoryId: 4,
+        name: "software",
+        creationDate: "2014-04-14"
+    },
+    {
+        categoryId: 5,
+        name: "love",
+        creationDate: "2012-09-07"
+    },
+    {
+        categoryId: 6,
+        name: "harmony",
+        creationDate: "2014-02-22"
+    },
+    {
+        categoryId: 7,
+        name: "nature",
+        creationDate: "2013-05-12"
+    },
+    {
+        categoryId: 8,
+        name: "happiness",
+        creationDate: "2019-03-01"
+    },
+    {
+        categoryId: 9,
+        name: "art",
+        creationDate: "2017-08-27"
+    },
+    {
+        categoryId: 10,
+        name: "music",
+        creationDate: "2014-11-30"
+    }
+
+]
