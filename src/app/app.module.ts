fd591782090486e98d970237452650598b5ae524
Aslıhan Hasar <aslihan_hasar@hotmail.com>
2023-07-16 06:09:01 +0300

    Create category table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserModule } from './user/user.module';
 import { PostModule } from './post/post.module';
+import { CategoryModule } from './category/category.module';
 import { ActionButtonComponent } from './action-button/action-button.component';
 import { NavbarComponent } from './navbar/navbar.component';
 @NgModule({
--- src/app/category/category-list/category-list.component.ts
+++ src/app/category/category-list/category-list.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Category } from '../category';
+import { CategoryService } from '../category.service';
 
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
   styleUrls: ['./category-list.component.css']
 })
-export class CategoryListComponent {
+export class CategoryListComponent implements OnInit {
+  categories: Category[] = [];
+  tableColumns: string[] = [];
+
+  constructor(
+    private categoryService: CategoryService,
+  ) {
+    if (this.categoryService.getCategories().length == 0) {
+      const newCategories: Category[] = [];
+      this.categoryService.setCategories(newCategories);
+    }
+    this.categories = this.categoryService.getCategories();
+  }
+
+  ngOnInit() {
+    this.categories = this.categoryService.getCategories();
+    this.tableColumns = this.categoryService.getPostProperties();
+  }
+
+  performDelete($event:Number){
+  
+  }
+
+  performDetails($event:Number){
+
+  }
 
 }
