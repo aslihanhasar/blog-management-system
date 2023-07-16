d53d759e7e1fb6f101b41b529e934cd34f642e65
Aslıhan Hasar <aslihan_hasar@hotmail.com>
2023-07-16 06:29:14 +0300

    Add delete() method

--- src/app/category/category.service.ts
+++ src/app/category/category.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Category } from './category';
 import { categories } from 'src/assets/categorypool';
-
+import { PostService } from '../post/post.service';
+import { Post } from '../post/post';
 @Injectable({
   providedIn: 'root'
 })
@@ -16,9 +17,22 @@ export class CategoryService {
     categories.push(...newCategories);
   }
 
-  constructor() { }
+  constructor(private postService: PostService) { }
 
    getPostProperties(): string[] {
     return Object.keys(categories[0]);
   }
+
+  deleteCategory($event: Number) {
+    if (this.postService.getPosts().length === 0){
+      const newPosts: Post[] = [];
+      this.postService.setPosts(newPosts);
+    }
+    if (this.postService.getPosts().length > 0 && this.postService.getPosts().some(post => post.categoryId === $event)) {
+      alert("You cannot delete a category with posts");
+    } else {
+      let dataCategories=this.getCategories() ;
+      dataCategories=categories.filter(category => category.categoryId !== $event);
+    }
+}
 }
