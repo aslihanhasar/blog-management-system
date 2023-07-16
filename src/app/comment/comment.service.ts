66e3cef0c4ff536e3b40f9a42b21bb5beb537e26
Aslıhan Hasar <aslihan_hasar@hotmail.com>
2023-07-16 16:04:10 +0300

    Create service methods

--- src/app/comment/comment.service.ts
+++ src/app/comment/comment.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { Comment } from './comment';
+import { comments } from 'src/assets/commentpool';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,25 @@ import { Injectable } from '@angular/core';
 export class CommentService {
 
   constructor() { }
+
+  getComments():Comment[]{
+    return comments;
+  }
+
+  setComments(newComments: Comment[]): void {
+    comments.length = 0;
+    comments.push(...newComments);
+  }
+
+  addComment(comment: Comment) {
+    comments.push(comment);
+  }
+
+  deleteComment($event: Number):void {
+    const index = comments.findIndex(c => c.commentId ===$event);
+    if (index !== -1) {
+      comments.splice(index, 1);
+    }
+  }
+  
 }
