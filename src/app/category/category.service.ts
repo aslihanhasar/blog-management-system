bac6a7657275bedbc9bcb8d8322cbd56c3631179
Aslıhan Hasar <aslihan_hasar@hotmail.com>
2023-07-16 05:58:58 +0300

    Add category set-get methods

--- src/app/category/category.service.ts
+++ src/app/category/category.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { Category } from './category';
+import { categories } from 'src/assets/categorypool';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
 
+  getCategories():Category[]{
+    return categories;
+  }
+
+  setCategories(newCategories: Category[]): void {
+    categories.length = 0;
+    categories.push(...newCategories);
+  }
+
   constructor() { }
+
+   getPostProperties(): string[] {
+    return Object.keys(categories[0]);
+  }
 }
