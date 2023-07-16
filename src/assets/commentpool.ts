e967f26f6045c30b6f1144520ca4feb657bddbea
Aslıhan Hasar <aslihan_hasar@hotmail.com>
2023-07-16 15:47:29 +0300

    Add comments data

---
+++ src/assets/commentpool.ts
@@ -0,0 +1,804 @@
+import { Comment } from "src/app/comment/comment"
+
+export const comments:Comment[]=[
+    {
+      commentId: 1,
+      postId: 70,
+      userId: 21,
+      comment: "Informative content.",
+      creationDate: "2022-03-28",
+      isConfirmed: false
+    },
+    {
+      commentId: 2,
+      postId: 9,
+      userId: 13,
+      comment: "Well written!",
+      creationDate: "2015-11-08",
+      isConfirmed: true
+    },
+    {
+      commentId: 3,
+      postId: 31,
+      userId: 22,
+      comment: "Nice job!",
+      creationDate: "2011-08-07",
+      isConfirmed: true
+    },
+    {
+      commentId: 4,
+      postId: 71,
+      userId: 18,
+      comment: "Interesting read!",
+      creationDate: "2014-10-31",
+      isConfirmed: true
+    },
+    {
+      commentId: 5,
+      postId: 10,
+      userId: 18,
+      comment: "Great post!",
+      creationDate: "2011-08-11",
+      isConfirmed: false
+    },
+    {
+      commentId: 6,
+      postId: 17,
+      userId: 16,
+      comment: "Interesting read!",
+      creationDate: "2016-05-26",
+      isConfirmed: false
+    },
+    {
+      commentId: 7,
+      postId: 77,
+      userId: 29,
+      comment: "Nice job!",
+      creationDate: "2017-09-26",
+      isConfirmed: false
+    },
+    {
+      commentId: 8,
+      postId: 6,
+      userId: 11,
+      comment: "Informative content.",
+      creationDate: "2023-05-06",
+      isConfirmed: true
+    },
+    {
+      commentId: 9,
+      postId: 50,
+      userId: 28,
+      comment: "Informative content.",
+      creationDate: "2010-05-27",
+      isConfirmed: true
+    },
+    {
+      commentId: 10,
+      postId: 98,
+      userId: 28,
+      comment: "Interesting read!",
+      creationDate: "2015-07-06",
+      isConfirmed: true
+    },
+    {
+      commentId: 11,
+      postId: 53,
+      userId: 4,
+      comment: "Nice job!",
+      creationDate: "2016-07-22",
+      isConfirmed: true
+    },
+    {
+      commentId: 12,
+      postId: 76,
+      userId: 16,
+      comment: "I disagree.",
+      creationDate: "2017-04-30",
+      isConfirmed: false
+    },
+    {
+      commentId: 13,
+      postId: 60,
+      userId: 9,
+      comment: "Interesting read!",
+      creationDate: "2011-01-17",
+      isConfirmed: true
+    },
+    {
+      commentId: 14,
+      postId: 71,
+      userId: 18,
+      comment: "Well written!",
+      creationDate: "2011-08-05",
+      isConfirmed: false
+    },
+    {
+      commentId: 15,
+      postId: 79,
+      userId: 6,
+      comment: "Informative content.",
+      creationDate: "2018-12-31",
+      isConfirmed: false
+    },
+    {
+      commentId: 16,
+      postId: 56,
+      userId: 19,
+      comment: "Informative content.",
+      creationDate: "2015-02-25",
+      isConfirmed: true
+    },
+    {
+      commentId: 17,
+      postId: 92,
+      userId: 26,
+      comment: "Well written!",
+      creationDate: "2018-12-21",
+      isConfirmed: false
+    },
+    {
+      commentId: 18,
+      postId: 62,
+      userId: 22,
+      comment: "Informative content.",
+      creationDate: "2023-02-17",
+      isConfirmed: true
+    },
+    {
+      commentId: 19,
+      postId: 32,
+      userId: 29,
+      comment: "Well written!",
+      creationDate: "2021-05-29",
+      isConfirmed: true
+    },
+    {
+      commentId: 20,
+      postId: 92,
+      userId: 19,
+      comment: "Interesting read!",
+      creationDate: "2018-01-12",
+      isConfirmed: false
+    },
+    {
+      commentId: 21,
+      postId: 53,
+      userId: 26,
+      comment: "Great post!",
+      creationDate: "2010-01-10",
+      isConfirmed: false
+    },
+    {
+      commentId: 22,
+      postId: 25,
+      userId: 22,
+      comment: "Well written!",
+      creationDate: "2019-02-04",
+      isConfirmed: true
+    },
+    {
+      commentId: 23,
+      postId: 54,
+      userId: 15,
+      comment: "Informative content.",
+      creationDate: "2014-06-02",
+      isConfirmed: false
+    },
+    {
+      commentId: 24,
+      postId: 92,
+      userId: 12,
+      comment: "I disagree.",
+      creationDate: "2010-07-10",
+      isConfirmed: false
+    },
+    {
+      commentId: 25,
+      postId: 18,
+      userId: 1,
+      comment: "Nice job!",
+      creationDate: "2014-04-09",
+      isConfirmed: false
+    },
+    {
+      commentId: 26,
+      postId: 54,
+      userId: 15,
+      comment: "Great post!",
+      creationDate: "2023-02-01",
+      isConfirmed: true
+    },
+    {
+      commentId: 27,
+      postId: 36,
+      userId: 21,
+      comment: "Informative content.",
+      creationDate: "2014-08-27",
+      isConfirmed: false
+    },
+    {
+      commentId: 28,
+      postId: 79,
+      userId: 22,
+      comment: "Great post!",
+      creationDate: "2016-01-25",
+      isConfirmed: false
+    },
+    {
+      commentId: 29,
+      postId: 78,
+      userId: 26,
+      comment: "Well written!",
+      creationDate: "2023-03-15",
+      isConfirmed: true
+    },
+    {
+      commentId: 30,
+      postId: 43,
+      userId: 30,
+      comment: "I disagree.",
+      creationDate: "2016-12-06",
+      isConfirmed: true
+    },
+    {
+      commentId: 31,
+      postId: 52,
+      userId: 18,
+      comment: "Great post!",
+      creationDate: "2018-11-07",
+      isConfirmed: false
+    },
+    {
+      commentId: 32,
+      postId: 86,
+      userId: 17,
+      comment: "I disagree.",
+      creationDate: "2016-04-30",
+      isConfirmed: true
+    },
+    {
+      commentId: 33,
+      postId: 43,
+      userId: 5,
+      comment: "Informative content.",
+      creationDate: "2014-05-08",
+      isConfirmed: false
+    },
+    {
+      commentId: 34,
+      postId: 20,
+      userId: 9,
+      comment: "Great post!",
+      creationDate: "2020-03-14",
+      isConfirmed: false
+    },
+    {
+      commentId: 35,
+      postId: 35,
+      userId: 1,
+      comment: "Interesting read!",
+      creationDate: "2015-05-25",
+      isConfirmed: false
+    },
+    {
+      commentId: 36,
+      postId: 15,
+      userId: 22,
+      comment: "Interesting read!",
+      creationDate: "2013-04-17",
+      isConfirmed: false
+    },
+    {
+      commentId: 37,
+      postId: 16,
+      userId: 12,
+      comment: "Informative content.",
+      creationDate: "2020-12-05",
+      isConfirmed: true
+    },
+    {
+      commentId: 38,
+      postId: 6,
+      userId: 6,
+      comment: "I disagree.",
+      creationDate: "2018-01-30",
+      isConfirmed: true
+    },
+    {
+      commentId: 39,
+      postId: 19,
+      userId: 10,
+      comment: "I disagree.",
+      creationDate: "2019-03-06",
+      isConfirmed: false
+    },
+    {
+      commentId: 40,
+      postId: 10,
+      userId: 27,
+      comment: "Informative content.",
+      creationDate: "2023-03-07",
+      isConfirmed: true
+    },
+    {
+      commentId: 41,
+      postId: 25,
+      userId: 18,
+      comment: "Interesting read!",
+      creationDate: "2017-02-25",
+      isConfirmed: true
+    },
+    {
+      commentId: 42,
+      postId: 36,
+      userId: 20,
+      comment: "Well written!",
+      creationDate: "2012-12-30",
+      isConfirmed: false
+    },
+    {
+      commentId: 43,
+      postId: 39,
+      userId: 30,
+      comment: "Interesting read!",
+      creationDate: "2014-09-01",
+      isConfirmed: false
+    },
+    {
+      commentId: 44,
+      postId: 37,
+      userId: 15,
+      comment: "Great post!",
+      creationDate: "2017-12-26",
+      isConfirmed: false
+    },
+    {
+      commentId: 45,
+      postId: 100,
+      userId: 25,
+      comment: "Informative content.",
+      creationDate: "2021-05-30",
+      isConfirmed: false
+    },
+    {
+      commentId: 46,
+      postId: 87,
+      userId: 8,
+      comment: "Interesting read!",
+      creationDate: "2010-12-06",
+      isConfirmed: false
+    },
+    {
+      commentId: 47,
+      postId: 10,
+      userId: 14,
+      comment: "Nice job!",
+      creationDate: "2011-08-04",
+      isConfirmed: true
+    },
+    {
+      commentId: 48,
+      postId: 41,
+      userId: 10,
+      comment: "Nice job!",
+      creationDate: "2014-03-07",
+      isConfirmed: false
+    },
+    {
+      commentId: 49,
+      postId: 46,
+      userId: 8,
+      comment: "Nice job!",
+      creationDate: "2018-01-29",
+      isConfirmed: true
+    },
+    {
+      commentId: 50,
+      postId: 77,
+      userId: 15,
+      comment: "Well written!",
+      creationDate: "2017-03-29",
+      isConfirmed: true
+    },
+    {
+      commentId: 51,
+      postId: 35,
+      userId: 18,
+      comment: "Informative content.",
+      creationDate: "2021-08-05",
+      isConfirmed: false
+    },
+    {
+      commentId: 52,
+      postId: 12,
+      userId: 12,
+      comment: "Nice job!",
+      creationDate: "2019-03-02",
+      isConfirmed: false
+    },
+    {
+      commentId: 53,
+      postId: 7,
+      userId: 2,
+      comment: "Well written!",
+      creationDate: "2014-06-16",
+      isConfirmed: false
+    },
+    {
+      commentId: 54,
+      postId: 26,
+      userId: 9,
+      comment: "Great post!",
+      creationDate: "2014-06-18",
+      isConfirmed: true
+    },
+    {
+      commentId: 55,
+      postId: 80,
+      userId: 12,
+      comment: "Interesting read!",
+      creationDate: "2018-04-10",
+      isConfirmed: true
+    },
+    {
+      commentId: 56,
+      postId: 90,
+      userId: 12,
+      comment: "Great post!",
+      creationDate: "2010-06-27",
+      isConfirmed: false
+    },
+    {
+      commentId: 57,
+      postId: 59,
+      userId: 16,
+      comment: "Informative content.",
+      creationDate: "2021-08-21",
+      isConfirmed: false
+    },
+    {
+      commentId: 58,
+      postId: 58,
+      userId: 16,
+      comment: "Informative content.",
+      creationDate: "2017-07-17",
+      isConfirmed: true
+    },
+    {
+      commentId: 59,
+      postId: 7,
+      userId: 20,
+      comment: "I disagree.",
+      creationDate: "2012-10-03",
+      isConfirmed: true
+    },
+    {
+      commentId: 60,
+      postId: 54,
+      userId: 18,
+      comment: "Great post!",
+      creationDate: "2023-01-25",
+      isConfirmed: false
+    },
+    {
+      commentId: 61,
+      postId: 96,
+      userId: 18,
+      comment: "Interesting read!",
+      creationDate: "2022-04-18",
+      isConfirmed: true
+    },
+    {
+      commentId: 62,
+      postId: 11,
+      userId: 21,
+      comment: "Great post!",
+      creationDate: "2019-07-16",
+      isConfirmed: true
+    },
+    {
+      commentId: 63,
+      postId: 7,
+      userId: 11,
+      comment: "Great post!",
+      creationDate: "2014-07-17",
+      isConfirmed: true
+    },
+    {
+      commentId: 64,
+      postId: 65,
+      userId: 1,
+      comment: "Interesting read!",
+      creationDate: "2010-12-18",
+      isConfirmed: false
+    },
+    {
+      commentId: 65,
+      postId: 43,
+      userId: 15,
+      comment: "Informative content.",
+      creationDate: "2022-09-13",
+      isConfirmed: true
+    },
+    {
+      commentId: 66,
+      postId: 20,
+      userId: 29,
+      comment: "I disagree.",
+      creationDate: "2016-07-01",
+      isConfirmed: false
+    },
+    {
+      commentId: 67,
+      postId: 53,
+      userId: 25,
+      comment: "I disagree.",
+      creationDate: "2019-03-12",
+      isConfirmed: false
+    },
+    {
+      commentId: 68,
+      postId: 85,
+      userId: 11,
+      comment: "Informative content.",
+      creationDate: "2012-12-18",
+      isConfirmed: true
+    },
+    {
+      commentId: 69,
+      postId: 85,
+      userId: 11,
+      comment: "Interesting read!",
+      creationDate: "2022-12-24",
+      isConfirmed: false
+    },
+    {
+      commentId: 70,
+      postId: 90,
+      userId: 15,
+      comment: "Nice job!",
+      creationDate: "2012-04-07",
+      isConfirmed: true
+    },
+    {
+      commentId: 71,
+      postId: 48,
+      userId: 10,
+      comment: "Informative content.",
+      creationDate: "2023-07-09",
+      isConfirmed: true
+    },
+    {
+      commentId: 72,
+      postId: 16,
+      userId: 4,
+      comment: "Great post!",
+      creationDate: "2010-01-09",
+      isConfirmed: false
+    },
+    {
+      commentId: 73,
+      postId: 71,
+      userId: 20,
+      comment: "Well written!",
+      creationDate: "2010-05-13",
+      isConfirmed: true
+    },
+    {
+      commentId: 74,
+      postId: 82,
+      userId: 15,
+      comment: "Informative content.",
+      creationDate: "2020-06-19",
+      isConfirmed: false
+    },
+    {
+      commentId: 75,
+      postId: 82,
+      userId: 15,
+      comment: "Great post!",
+      creationDate: "2016-05-02",
+      isConfirmed: true
+    },
+    {
+      commentId: 76,
+      postId: 72,
+      userId: 10,
+      comment: "Well written!",
+      creationDate: "2021-06-25",
+      isConfirmed: true
+    },
+    {
+      commentId: 77,
+      postId: 91,
+      userId: 15,
+      comment: "I disagree.",
+      creationDate: "2016-03-28",
+      isConfirmed: false
+    },
+    {
+      commentId: 78,
+      postId: 24,
+      userId: 28,
+      comment: "Interesting read!",
+      creationDate: "2011-01-10",
+      isConfirmed: false
+    },
+    {
+      commentId: 79,
+      postId: 86,
+      userId: 13,
+      comment: "Informative content.",
+      creationDate: "2019-10-25",
+      isConfirmed: false
+    },
+    {
+      commentId: 80,
+      postId: 64,
+      userId: 29,
+      comment: "Informative content.",
+      creationDate: "2022-08-28",
+      isConfirmed: false
+    },
+    {
+      commentId: 81,
+      postId: 7,
+      userId: 9,
+      comment: "Nice job!",
+      creationDate: "2015-07-29",
+      isConfirmed: true
+    },
+    {
+      commentId: 82,
+      postId: 95,
+      userId: 4,
+      comment: "Interesting read!",
+      creationDate: "2016-10-17",
+      isConfirmed: true
+    },
+    {
+      commentId: 83,
+      postId: 58,
+      userId: 18,
+      comment: "I disagree.",
+      creationDate: "2012-07-20",
+      isConfirmed: false
+    },
+    {
+      commentId: 84,
+      postId: 73,
+      userId: 25,
+      comment: "Nice job!",
+      creationDate: "2017-01-07",
+      isConfirmed: true
+    },
+    {
+      commentId: 85,
+      postId: 35,
+      userId: 5,
+      comment: "Interesting read!",
+      creationDate: "2020-06-12",
+      isConfirmed: false
+    },
+    {
+      commentId: 86,
+      postId: 93,
+      userId: 4,
+      comment: "Informative content.",
+      creationDate: "2014-11-27",
+      isConfirmed: false
+    },
+    {
+      commentId: 87,
+      postId: 42,
+      userId: 7,
+      comment: "Great post!",
+      creationDate: "2014-03-19",
+      isConfirmed: true
+    },
+    {
+      commentId: 88,
+      postId: 41,
+      userId: 18,
+      comment: "I disagree.",
+      creationDate: "2016-03-10",
+      isConfirmed: false
+    },
+    {
+      commentId: 89,
+      postId: 87,
+      userId: 17,
+      comment: "Informative content.",
+      creationDate: "2017-06-20",
+      isConfirmed: false
+    },
+    {
+      commentId: 90,
+      postId: 90,
+      userId: 10,
+      comment: "Nice job!",
+      creationDate: "2020-03-06",
+      isConfirmed: false
+    },
+    {
+      commentId: 91,
+      postId: 33,
+      userId: 21,
+      comment: "Nice job!",
+      creationDate: "2015-02-02",
+      isConfirmed: false
+    },
+    {
+      commentId: 92,
+      postId: 39,
+      userId: 24,
+      comment: "Interesting read!",
+      creationDate: "2022-05-23",
+      isConfirmed: false
+    },
+    {
+      commentId: 93,
+      postId: 54,
+      userId: 19,
+      comment: "Great post!",
+      creationDate: "2023-03-13",
+      isConfirmed: false
+    },
+    {
+      commentId: 94,
+      postId: 83,
+      userId: 14,
+      comment: "Great post!",
+      creationDate: "2022-01-19",
+      isConfirmed: false
+    },
+    {
+      commentId: 95,
+      postId: 62,
+      userId: 8,
+      comment: "Interesting read!",
+      creationDate: "2021-12-16",
+      isConfirmed: false
+    },
+    {
+      commentId: 96,
+      postId: 50,
+      userId: 14,
+      comment: "Well written!",
+      creationDate: "2012-09-22",
+      isConfirmed: true
+    },
+    {
+      commentId: 97,
+      postId: 63,
+      userId: 17,
+      comment: "Informative content.",
+      creationDate: "2021-05-30",
+      isConfirmed: false
+    },
+    {
+      commentId: 98,
+      postId: 83,
+      userId: 21,
+      comment: "Informative content.",
+      creationDate: "2010-02-28",
+      isConfirmed: true
+    },
+    {
+      commentId: 99,
+      postId: 31,
+      userId: 14,
+      comment: "Informative content.",
+      creationDate: "2022-01-18",
+      isConfirmed: true
+    },
+    {
+      commentId: 100,
+      postId: 97,
+      userId: 28,
+      comment: "Informative content.",
+      creationDate: "2012-10-20",
+      isConfirmed: true
+    }
+  ]
